e4d86131d33ded093fde12ba19e0ab910d88bdef
Alexey Dikarev <mailsub@yandex.ru>
2020-05-11 03:03:55 +0300

    added progress bars, components sectiontitle and section subtitle, tasks.txt

--- components/StatisticsCard.vue
+++ components/StatisticsCard.vue
@@ -1,15 +1,33 @@
 <template>
   <div class="statistics-card">
     <p class="statistics-card__text">{{ data.text }}</p>
-    <div class="statistics-card__bar"></div>
+    <div class="statistics-card__bar-container">
+      <progress-bar
+        v-if="data.barType === '1'"
+        :value="data.value"
+        :max-value="data.maxValue"
+      />
+      <progress-bar-double
+        v-if="data.barType === '2'"
+        :previous-value="data.previousValue"
+        :current-value="data.currentValue"
+        :max-value="data.maxValue"
+      />
+    </div>
     <p class="statistics-card__digits">{{ data.digits }}</p>
     <p class="statistics-card__source">{{ data.source }}</p>
   </div>
 </template>
 
 <script>
+import ProgressBar from '@/components/ui/ProgressBar';
+import ProgressBarDouble from '@/components/ui/ProgressBarDouble.vue';
 export default {
   props: ['data'],
+  components: {
+    'progress-bar': ProgressBar,
+    'progress-bar-double': ProgressBarDouble,
+  },
 };
 </script>
 
@@ -27,12 +45,12 @@ export default {
   font-size: 12px;
   line-height: 16px;
   position: absolute;
+  width: calc(100% - 20px * 2);
 }
 
-.statistics-card__bar {
+.statistics-card__bar-container {
   width: 100%;
   height: 40px;
-  background: #f4f4f4;
   margin-top: 144px;
 }
 
---
+++ components/ui/SectionTitle.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="section-title">
+    <h3 class="section-title__text">
+      <slot></slot>
+    </h3>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.section-title {
+  color: #000000;
+}
+.section-title__text {
+  max-width: 413px;
+  font-weight: 600;
+  font-size: 32px;
+  line-height: 36px;
+}
+</style>

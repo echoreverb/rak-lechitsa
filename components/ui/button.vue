75030c5afd5b517a4451d57a4d8ebaabf106de94
Alexey Dikarev <mailsub@yandex.ru>
2020-05-11 19:07:10 +0300

    local commit

--- components/ui/button.vue
+++ components/ui/button.vue
@@ -1,28 +1,50 @@
 <template>
-  <button class="button" type="button">
-    <slot></slot>
+  <button :class="`button button_size_${size}`">
+    {{ text }}
   </button>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ['text', 'size'],
+};
 </script>
 
 <style scoped>
 .button {
-  width: 280px;
-  height: 52px;
-  background: #613a93;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   font-weight: 500;
   font-size: 16px;
   line-height: 19px;
+  text-align: center;
+  cursor: pointer;
+  border: none;
+}
+.button_size_sm {
+  padding: 9px 20px;
+  background-color: #fbfbfb;
+}
+.button_size_sm:hover {
+  background-color: #f8f8f8;
+}
+.button_active {
+  background-color: #f4f4f4;
+}
+.button_size_md {
+  padding: 17px 44px;
+  width: 280px;
+  background-color: #613a93;
   color: #fff;
 }
-
-.button:hover {
-  cursor: pointer;
+.button_size_md:hover {
+  opacity: 0.9;
+}
+.button_size_lg {
+  width: 100%;
+  padding: 31px 0;
+  background-color: #fbfbfb;
+  border: none;
+}
+.button_size_lg:hover {
+  background-color: #f8f8f8;
 }
 </style>
